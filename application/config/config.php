bd2e08e31ac141ce2a53c4e6aa3ae539ed4d5e21
604942578 <604942578@qq.com>
2016-07-09 21:45:34 +0800

    MW

--- application/config/config.php
+++ application/config/config.php
@@ -108,7 +108,7 @@ $config['enable_hooks'] = FALSE;
 | http://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'MJ_';
+$config['subclass_prefix'] = 'MW_';
 
 /*
 |--------------------------------------------------------------------------
