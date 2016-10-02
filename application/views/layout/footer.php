7e1ad031dd740671ec2a54313c223e902aa8783d
604942578 <604942578@qq.com>
2016-10-02 20:51:57 +0800

    登陆页

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -2,12 +2,12 @@
 	<div class="bottom-nav">
 		<ul class="clearfix">
 			<li><a target="_blank" href="<?php echo $this->config->main_base_url?>">妙处网首页</a></li>
-			<li><a target="_blank" href="<?php echo $this->config->help_url;?>">公司简介</a></li>
-			<li><a target="_blank" href="<?php echo $this->config->help_url;?>">招商加盟</a></li>
-			<li><a target="_blank" href="<?php echo $this->config->help_url ;?>">服务申请</a></li>
-			<li><a target="_blank" href="<?php echo $this->config->help_url;?>">供应合作</a></li>
-			<li><a target="_blank" href="<?php echo $this->config->help_url;?>">加盟分销</a></li>
-			<li><a target="_blank" href="<?php echo $this->config->help_url;?>">联系我们</a></li>
+			<li><a target="_blank" href="<?php echo $this->config->help_url.'help_center/index/26.html';?>">公司简介</a></li>
+			<li><a target="_blank" href="<?php echo $this->config->help_url.'help_center/index/27.html';?>">招商加盟</a></li>
+			<li><a target="_blank" href="<?php echo $this->config->help_url.'help_center/index/9.html' ;?>">购物流程</a></li>
+			<li><a target="_blank" href="<?php echo $this->config->help_url.'help_center/index/28.html';?>">常见问题</a></li>
+			<li><a target="_blank" href="<?php echo $this->config->help_url.'help_center/index/16.html';?>">隐私保护</a></li>
+			<li><a target="_blank" href="<?php echo $this->config->help_url.'help_center/index/22.html';?>">正品保证</a></li>
 			<li class="last"><a target="_blank" href="<?php echo $this->config->help_url;?>">帮助中心</a></li>
 		</ul>
 	</div>
