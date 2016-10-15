9dbd934123628f8b46ebbdb22e4f69fc8fe8098b
Rabin <zhangkunch@qq.com>
2016-10-15 23:14:21 +0800

    修改文字

--- application/views/register/regsuccess.php
+++ application/views/register/regsuccess.php
@@ -1,8 +1,8 @@
 <?php $this->load->view('layout/header');?>
 <div class="success-register">
     <div class="success-text">
-        <p class="suc-your">恭喜您，您已经成功注册贝竹一站通！</p>
-        <p class="suc-zhang">您在贝竹一站通的登录账号为<i><?php echo $username;?></i>请妥善保管您的账号资料。</p>
+        <p class="suc-your">恭喜您，您已经成功注册妙处网会员！</p>
+        <p class="suc-zhang">您在妙处网的登录账号为<i><?php echo $username;?></i>请妥善保管您的账号资料。</p>
         <div class="success-button clearfix">
             <span>现在您可以：</span>
             <a href="<?php echo $this->config->main_base_url;?>" class="btn btn-success">立即购物</a>
